4e23095c714b37b91e3b432922d73ed39cf10dfa
npokrandt <npokrandt919@gmail.com>
2023-10-10 23:47:50 -0500

    adds get routes for the user model

--- controllers/userRoutes.js
+++ controllers/userRoutes.js
@@ -1,8 +1,27 @@
 const router = require('express').Router()
+const { User } = require('../models')
 
-router.get('/', (req, res) => {
-    console.log(req)
-    res.end()
+router.get('/', async (req, res) => {
+    try {
+        //console.log(req)
+        const users = await User.find({})
+
+        res.status(200).json(users)
+    }  catch (err) {
+        res.status(400).json(err);
+    }   
+})
+
+router.get('/:user_id', async (req, res) => {
+    try {
+        const users = await User.findById({
+            _id: req.params.user_id
+        })
+
+        res.status(200).json(users)
+    }  catch (err) {
+        res.status(400).json(err);
+    }   
 })
 
 module.exports = router
