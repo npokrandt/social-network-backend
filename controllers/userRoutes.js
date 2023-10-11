8407263433ff2bf386291c6b30693c7c0b15360a
npokrandt <npokrandt919@gmail.com>
2023-10-10 23:57:16 -0500

    adds post route to add new users

--- controllers/userRoutes.js
+++ controllers/userRoutes.js
@@ -14,14 +14,27 @@ router.get('/', async (req, res) => {
 
 router.get('/:user_id', async (req, res) => {
     try {
-        const users = await User.findById({
+        const user = await User.findById({
             _id: req.params.user_id
         })
 
-        res.status(200).json(users)
+        res.status(200).json(user)
     }  catch (err) {
         res.status(400).json(err);
     }   
+})
+
+router.post('/', async (req, res) => {
+    try {
+        const newUser = await User.create({
+            username: req.body.username,
+            email: req.body.email
+        })
+
+        res.status(201).json(newUser)
+    }  catch (err) {
+        res.status(400).json(err);
+    } 
 })
 
 module.exports = router
