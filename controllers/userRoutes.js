9ff42811bbae92620b2e579f2472a0e84649fd96
npokrandt <npokrandt919@gmail.com>
2023-10-11 12:01:54 -0500

    adds the rest of the routes for users, aside from friend routes

--- controllers/userRoutes.js
+++ controllers/userRoutes.js
@@ -18,6 +18,13 @@ router.get('/:user_id', async (req, res) => {
             _id: req.params.user_id
         })
 
+
+        if (user !== null){
+    
+            res.status(200).json(user)
+        } else {
+            res.status(404).send('user not found')
+        }
         res.status(200).json(user)
     }  catch (err) {
         res.status(400).json(err);
@@ -36,5 +43,47 @@ router.post('/', async (req, res) => {
         res.status(400).json(err);
     } 
 })
+
+router.put('/:user_id', async (req, res) => {
+    try {
+        const updatedUser = await User.findByIdAndUpdate(
+            {
+                _id: req.params.user_id
+            },
+            {
+                username: req.body.username,
+                email: req.body.email
+            }
+        )
+
+        if (updatedUser !== null){
+    
+            res.status(200).json(updatedUser)
+        } else {
+            res.status(404).send('user not found')
+        }
+    }  catch (err) {
+        res.status(400).json(err);
+    } 
+})
+
+router.delete('/:user_id', async (req, res) => {
+    try {
+        const deletedUser = await User.findByIdAndDelete({
+            _id: req.params.user_id
+        })
+
+        if (deletedUser !== null){
+    
+            res.status(200).json(deletedUser)
+        } else {
+            res.status(404).send('user not found')
+        }
+    }  catch (err) {
+        res.status(400).json(err);
+    } 
+})
+
+//routes to edit friend list
 
 module.exports = router
