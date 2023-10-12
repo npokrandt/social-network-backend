fd0ff51bdca50c0b202bf5f9f13de77ba9535d04
npokrandt <npokrandt919@gmail.com>
2023-10-12 15:16:36 -0500

    adds the remaining thought routes

--- controllers/thoughtRoutes.js
+++ controllers/thoughtRoutes.js
@@ -11,6 +11,24 @@ router.get('/', async (req, res) => {
     } 
 })
 
+router.get('/:thought_id', async (req, res) => {
+    try {
+        const thought = await Thought.findById({
+            _id: req.params.thought_id
+        })
+
+        if (thought !== null){
+    
+            res.status(200).json(thought)
+        } else {
+            res.status(404).send('thought not found')
+        }
+
+    } catch (err) {
+        res.status(400).json(err);
+    } 
+})
+
 router.post('/', async (req, res) => {
     try {
         const newThought = await Thought.create({
@@ -29,5 +47,51 @@ router.post('/', async (req, res) => {
     }  catch (err) {
         res.status(400).json(err); 
     }
+})
+
+router.put('/:thought_id', async (req, res) => {
+    try {
+        const updateThought = await Thought.findByIdAndUpdate({
+            _id: req.params.thought_id
+        },
+        {
+            thoughtText: req.body.thoughtText,
+        })
+
+        if (updateThought !== null){
+    
+            res.status(200).json(updateThought)
+        } else {
+            res.status(404).send('thought not found')
+        }
+
+    } catch (err) {
+        res.status(400).json(err);
+    } 
+})
+
+router.delete('/:thought_id', async (req, res) => {
+    try {
+        const deleteThought = await Thought.findByIdAndDelete({
+            _id: req.params.thought_id
+        })
+
+        if (deleteThought !== null){
+
+            const user = await User.findOneAndUpdate({
+                username: deleteThought.username
+            },
+            {
+                $pull: {'thoughts': req.params.thought_id}
+            })
+    
+            res.status(200).json({deleteThought, user})
+        } else {
+            res.status(404).send('thought not found')
+        }
+
+    } catch (err) {
+        res.status(400).json(err);
+    } 
 })
 module.exports = router
